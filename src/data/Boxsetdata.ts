13a72175f27dbc587841deab508fe9fa5d490b69
pawataven <pawatwantrong@gmail.com>
2026-09-26 21:01:18 +0700

    feat: add homepage layout, product carousel, boxset components, and data

--- src/data/Boxsetdata.ts
+++ src/data/Boxsetdata.ts
@@ -17,7 +17,7 @@ export interface Boxset {
 export const boxsets: Boxset[] = [
   {
     image: Sokutai,
-    title: "FLOURIST : The Sokutai Collection",
+    title: "The Sokutai Collection",
     description: "The Ultimate Ceremonial Matcha Experience สัมผัสสุนทรียภาพแห่งการชงชาชั้นสูง ผ่านบรรจุภัณฑ์ที่ได้รับแรงบันดาลใจจาก โซคุไต (Sokutai) เครื่องแต่งกายชั้นสูงในราชสำนักญี่ปุ่นโบราณ ถอดรหัสโครงสร้างการซ้อนทับของเลเยอร์ผ้าทอมือและลวดลายมงคล สู่ดีไซน์ร่วมสมัยสไตล์ Quiet Luxury ที่รวบรวมอุปกรณ์ชงชาพิถีพิถันไว้ในกล่องเดียว",
     prices: [
       { size: "ราคา", price: "1750 บาท" },
